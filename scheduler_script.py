04bd7c0a5220a02cbcd5d04542dcc6e34fd75c7a
Erick Campolina <erick.campolina@hotmart.com>
2024-07-30 22:15:18 -0300

    feat: scheduler script

--- scheduler_script.py
+++ scheduler_script.py
@@ -3,20 +3,19 @@
 import time
 import random
 import json
+import threading
+import os
+
 from kubernetes import client, config, watch
 
 config.load_kube_config(config_file='/etc/kubernetes/admin.conf')
 v1=client.CoreV1Api()
 
+delay_thresold = 50
+nodes = ['worker1.vrk8s2.ilabt-imec-be.wall2.ilabt.iminds.be', 'worker2.vrk8s2.ilabt-imec-be.wall2.ilabt.iminds.be', 'worker3.vrk8s2.ilabt-imec-be.wall2.ilabt.iminds.be']
 scheduler_name = "vrscheduler"
-
-def nodes_available():
-    ready_nodes = []
-    for n in v1.list_node().items:
-            for status in n.status.conditions:
-                if status.status == "True" and status.type == "Ready":
-                    ready_nodes.append(n.metadata.name)
-    return ready_nodes
+nodes_delay = [85, 70, 34]
+isSchedulerActive = False
 
 def scheduler(pod_name, node_name, namespace="default"):
     metadata = client.V1ObjectMeta(name=pod_name)
@@ -29,29 +28,82 @@ def scheduler(pod_name, node_name, namespace="default"):
     body = client.V1Binding(metadata=metadata,target=target)
 
     try:
-        res = v1.create_namespaced_binding(namespace, body)
+        res = v1.create_namespaced_binding(namespace=namespace, body=body)
         if res:
             print(f"POD: {pod_name} scheduled and place on node {node_name}") 
     except Exception as a:
         print("Exception when calling CoreV1Api->create_namespaced_binding: %s\n" % a)
         pass
 
+def activate_scheduler_after_one_minute():
+    global isSchedulerActive
+    time.sleep(60)
+    isSchedulerActive = True
+    print('SCHEDULER ACTIVATED: ', isSchedulerActive)
+
+def get_good_nodes():
+    good_nodes = []
+
+    for index, value in enumerate(nodes_delay):
+        if value <= delay_thresold:
+            good_nodes.append(nodes[index])
+
+    return good_nodes
+
+def get_bad_nodes():
+    bad_nodes = []
+
+    for index, value in enumerate(nodes_delay):
+        if value > delay_thresold:
+            bad_nodes.append(nodes[index])
+
+    return bad_nodes
+
+def delete_pod_from_bad_node(bad_nodes, pod_name, current_node_name):
+    global isSchedulerActive
+    
+    if isSchedulerActive == False:
+        return
+
+    if current_node_name in bad_nodes:
+        os.system('export KUBECONFIG=/etc/kubernetes/admin.conf')
+        os.system(f'kubectl delete pods {pod_name}')
+        print(f'Pod {pod_name} has been deleted from node {current_node_name}!')
+    
 def main():
     print("SCHEDULER STARTED")
+
+    global isSchedulerActive
+    thread = threading.Thread(target=activate_scheduler_after_one_minute)
+    thread.start()
+
     w = watch.Watch()
+
+    good_nodes = get_good_nodes()
+    bad_nodes = get_bad_nodes()
+
+    print("GOOD NODES: ", good_nodes)
+    print("BAD NODES: ", bad_nodes)
+
     for event in w.stream(v1.list_namespaced_pod, namespace="default"):
-        print(f"EVENT: {event['type']}; {event['object'].kind}; {event['object'].status.phase}; {event['object'].metadata.name}; {event['object'].spec.scheduler_name}")
-        print(f"RESULT: {event['object'].status.phase == 'Pending' and event['object'].spec.scheduler_name == scheduler_name}") 
-        if event['object'].status.phase == "Pending" and event['object'].spec.scheduler_name == scheduler_name:
+        current_pod = event['object']
+
+        print(f"EVENT: {event['type']}; {current_pod.kind}; {current_pod.status.phase}; {current_pod.metadata.name}; {current_pod.spec.scheduler_name}")
+        print(f"RESULT: {current_pod.status.phase == 'Pending' and current_pod.spec.scheduler_name == scheduler_name}") 
+        
+        delete_pod_from_bad_node(bad_nodes, current_pod.metadata.name, current_pod.spec.node_name)
+
+        if current_pod.status.phase == "Pending" and current_pod.spec.scheduler_name == scheduler_name:
             try:
-                nodes = nodes_available()
-                if not nodes:
-                    print("No available nodes.")
-                    continue
-                selectedNode = random.choice(nodes)
-                print(selectedNode)
-                res = scheduler(event['object'].metadata.name, selectedNode)
-                print(f"Scheduled pod {event['object'].metadata.name} on node {selectedNode}.")
+                if isSchedulerActive == False:
+                    selectedNode = random.choice(nodes)
+                else:
+                    if len(good_nodes) == 0:
+                        continue
+                    else:
+                        selectedNode = random.choice(good_nodes)
+                res = scheduler(current_pod.metadata.name, selectedNode)
+                print(f"Scheduled pod {current_pod.metadata.name} on node {selectedNode}.")
             except client.rest.ApiException as e:
                 print (json.loads(e.body)['message'])
                 pass
